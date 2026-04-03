ede0bd98a3835b1704465d688675940bd4a5dc7a
Yatharth <yatharthmachhar9@gmail.com>
2026-04-04 03:44:06 +0530

    Fix memory leak and port binding for Render deployment

--- autotagger_backend/server.py
+++ autotagger_backend/server.py
@@ -190,4 +190,5 @@ def result_file(filename):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
